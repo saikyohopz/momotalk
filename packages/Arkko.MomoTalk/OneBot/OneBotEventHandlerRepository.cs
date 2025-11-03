57705a88b22973f3bd55af42f28cd99b1c5601c5
polyarkk <1781176460@qq.com>
2025-11-03 17:40:50 +0800

    friend poke event

--- packages/Arkko.MomoTalk/OneBot/OneBotEventHandlerRepository.cs
+++ packages/Arkko.MomoTalk/OneBot/OneBotEventHandlerRepository.cs
@@ -6,9 +6,34 @@ namespace Arkko.MomoTalk.OneBot;
 
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
 public class OneBotEventHandlerRepository {
-    private readonly ILogger<OneBotEventHandlerRepository> _logger;
-    
     private readonly Dictionary<Type, Action<EventBase>> _eventInvokers = [];
+
+    private readonly ILogger<OneBotEventHandlerRepository> _logger;
+
+    internal OneBotEventHandlerRepository(MomoTalk momoTalk, ILoggerFactory loggerFactory) {
+        _logger = loggerFactory.CreateLogger<OneBotEventHandlerRepository>();
+
+        RegisterEvent((EventMessagePrivate ev) => EventMessagePrivate?.Invoke(momoTalk, ev));
+        RegisterEvent((EventMessageGroup ev) => EventMessageGroup?.Invoke(momoTalk, ev));
+        RegisterEvent((EventConnect ev) => EventConnect?.Invoke(momoTalk, ev));
+        RegisterEvent((EventOneBotDisable ev) => EventOneBotDisable?.Invoke(momoTalk, ev));
+        RegisterEvent((EventOneBotEnable ev) => EventOneBotEnable?.Invoke(momoTalk, ev));
+        RegisterEvent((EventHeartbeat ev) => EventHeartbeat?.Invoke(momoTalk, ev));
+        RegisterEvent((EventFriendAdd ev) => EventFriendAdd?.Invoke(momoTalk, ev));
+        RegisterEvent((EventFriendRecall ev) => EventFriendRecall?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupAdmin ev) => EventGroupAdmin?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupBan ev) => EventGroupBan?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupMemberDecrease ev) => EventGroupDecrease?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupHonorChange ev) => EventGroupHonorChange?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupMemberIncrease ev) => EventGroupIncrease?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupLuckyKing ev) => EventGroupLuckyKing?.Invoke(momoTalk, ev));
+        RegisterEvent((EventPokeGroup ev) => EventPokeGroup?.Invoke(momoTalk, ev));
+        RegisterEvent((EventPokeFriend ev) => EventPokeFriend?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupRecall ev) => EventGroupRecall?.Invoke(momoTalk, ev));
+        RegisterEvent((EventGroupUpload ev) => EventGroupUpload?.Invoke(momoTalk, ev));
+        RegisterEvent((EventRequestFriend ev) => EventRequestFriend?.Invoke(momoTalk, ev));
+        RegisterEvent((EventRequestGroup ev) => EventRequestGroup?.Invoke(momoTalk, ev));
+    }
 
     public event ObAsyncEventHandler<EventMessagePrivate>? EventMessagePrivate;
     public event ObAsyncEventHandler<EventMessageGroup>? EventMessageGroup;
@@ -24,35 +49,12 @@ public class OneBotEventHandlerRepository {
     public event ObAsyncEventHandler<EventGroupHonorChange>? EventGroupHonorChange;
     public event ObAsyncEventHandler<EventGroupMemberIncrease>? EventGroupIncrease;
     public event ObAsyncEventHandler<EventGroupLuckyKing>? EventGroupLuckyKing;
-    public event ObAsyncEventHandler<EventPokeGroup>? EventGroupPoke;
+    public event ObAsyncEventHandler<EventPokeGroup>? EventPokeGroup;
+    public event ObAsyncEventHandler<EventPokeFriend>? EventPokeFriend;
     public event ObAsyncEventHandler<EventGroupRecall>? EventGroupRecall;
     public event ObAsyncEventHandler<EventGroupUpload>? EventGroupUpload;
     public event ObAsyncEventHandler<EventRequestFriend>? EventRequestFriend;
     public event ObAsyncEventHandler<EventRequestGroup>? EventRequestGroup;
-
-    internal OneBotEventHandlerRepository(MomoTalk momoTalk, ILoggerFactory loggerFactory) {
-        _logger = loggerFactory.CreateLogger<OneBotEventHandlerRepository>();
-        
-        RegisterEvent((EventMessagePrivate ev) => EventMessagePrivate?.Invoke(momoTalk, ev));
-        RegisterEvent((EventMessageGroup ev) => EventMessageGroup?.Invoke(momoTalk, ev));
-        RegisterEvent((EventConnect ev) => EventConnect?.Invoke(momoTalk, ev));
-        RegisterEvent((EventOneBotDisable ev) => EventOneBotDisable?.Invoke(momoTalk, ev));
-        RegisterEvent((EventOneBotEnable ev) => EventOneBotEnable?.Invoke(momoTalk, ev));
-        RegisterEvent((EventHeartbeat ev) => EventHeartbeat?.Invoke(momoTalk, ev));
-        RegisterEvent((EventFriendAdd ev) => EventFriendAdd?.Invoke(momoTalk, ev));
-        RegisterEvent((EventFriendRecall ev) => EventFriendRecall?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupAdmin ev) => EventGroupAdmin?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupBan ev) => EventGroupBan?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupMemberDecrease ev) => EventGroupDecrease?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupHonorChange ev) => EventGroupHonorChange?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupMemberIncrease ev) =>  EventGroupIncrease?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupLuckyKing ev) => EventGroupLuckyKing?.Invoke(momoTalk, ev));
-        RegisterEvent((EventPokeGroup ev) => EventGroupPoke?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupRecall ev) => EventGroupRecall?.Invoke(momoTalk, ev));
-        RegisterEvent((EventGroupUpload ev) => EventGroupUpload?.Invoke(momoTalk, ev));
-        RegisterEvent((EventRequestFriend ev) => EventRequestFriend?.Invoke(momoTalk, ev));
-        RegisterEvent((EventRequestGroup ev) => EventRequestGroup?.Invoke(momoTalk, ev));
-    }
 
     private void RegisterEvent<TEvent>(Action<TEvent> invoker) where TEvent : EventBase {
         _eventInvokers[typeof(TEvent)] = ev => invoker.Invoke((TEvent)ev);
