5a6388986f018f1db0f89ebe6123ab0b2e29bb76
polyarkk <1781176460@qq.com>
2025-11-14 12:29:54 +0800

    objutil tryget

--- packages/Arkko.MomoTalk.Foundation/Utils/ObjectUtils.cs
+++ packages/Arkko.MomoTalk.Foundation/Utils/ObjectUtils.cs
@@ -4,6 +4,12 @@ public static class ObjectUtils {
     public static bool TryGet<T>(this T? o, out T? v) {
         v = o;
 
-        return o != null;
+        return v != null;
+    }
+
+    public static Task<bool> TryGetAsync<T>(this Task<T?> o, out T? v) {
+        v = o.ConfigureAwait(false).GetAwaiter().GetResult();
+
+        return Task.FromResult(v != null);
     }
 }
